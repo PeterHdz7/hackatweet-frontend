0812f999a5c7e8f28de1c5f9961e9e250a5d4729
martin ballet <estebanballet@gmail.com>
2024-02-15 19:36:18 +0100

    HomePageLeftSide

--- components/Home.js
+++ components/Home.js
@@ -1,17 +1,29 @@
-import styles from '../styles/Home.module.css'
+import styles from "../styles/Home.module.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTwitter } from "@fortawesome/free-brands-svg-icons";
+import { faCircleUser } from "@fortawesome/free-regular-svg-icons";
 
 function Home() {
   return (
     <div className={styles.main}>
-      <div>
-hd
+      <div style={{ flex: 1 }} className={styles.leftSide}>
+        <FontAwesomeIcon icon={faTwitter} className={styles.top} />
+        <div>
+          <div className={styles.bottom}>
+            <div className={styles.icone}>
+              <FontAwesomeIcon icon={faCircleUser} />
+            </div>
+
+            <div>
+              <div className={styles.name} >John</div>
+              <div>@JohnCena</div>
+            </div>
+          </div>
+          <button className={styles.userSection}>logout</button>
+        </div>
       </div>
-      <div>
-hf
-      </div>
-      <div>
-wc
-      </div>
+      <div style={{ flex: 2, border: "1px solid white" }}>hf</div>
+      <div style={{ flex: 1 }}>wc</div>
     </div>
   );
 }
